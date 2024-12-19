97454d2e16433f5f5c0b5a0695efa0d8febffd15
rahul <rahulbhargavrgk@gmail.com>
2024-12-19 20:25:07 +0530

    execution router modified

--- backend/Routers/executeRouter.js
+++ backend/Routers/executeRouter.js
@@ -3,8 +3,8 @@ import run  from '../Controllers/runController.js';
 import { ensureAuthenticated } from '../Middleware/authenticationMiddleware.js';
 const executeRouter = express.Router();
 
-// executeRouter.post("/run",ensureAuthenticated,run)
-executeRouter.post('/run',run)
+executeRouter.post("/run",ensureAuthenticated,run)
+// executeRouter.post('/run',run)
 
 export {executeRouter}
 
